54e9b0388dd6bc0f949dfb4ffb6ced38bd587cab
jo675 <johnny.agosile@outlook.com>
2021-12-03 14:41:05 +0100

    Initial commit

---
+++ git-config-github/git-config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Welcome to git config"
+echo "*** Please tell me who you are?"
+
+echo "what is your name?"
+read name
+git config --global user.name "$name"
+
+echo "what is your e-mail?"
+read email
+git config --global user.email "$mail"
+
+echo "what is your username on GitHub?"
+read GitHubName
+echo "what is your GitHub token?"
+read token
+
+echo https://$GitHubName:$token@github.com >> ~/.git-credentials
+
+echo "Your git is configured and you may now begin pushing to GitHub"
